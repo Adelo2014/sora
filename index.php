70bab54a498d9e7ff3bd15b8185460dff9cfa576
Adelo2014 <68045623+Adelo2014@users.noreply.github.com>
2020-07-09 01:12:18 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html >
+<html class="no-js" lang="en"><!--<![endif]-->
+<head>
+
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.13.1/css/all.css" integrity="sha384-B9BoFFAuBaCfqw6lxWBZrhg/z4NkwqdBci+E+Sc2XlK/Rz25RYn8Fetb+Aw5irxa" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+
+	<style>
+
+		body {
+			position: relative;
+			background: url('https://wallpaperplay.com/walls/full/9/5/3/86847.jpg');
+		}
+
+		.container {
+			background: rgba(17,17,17,0.8);
+			position: absolute;
+			top: 0;
+			right: 0;
+		}
+
+		p,h1 {
+			color: #fff;
+			font-family: 'Roboto', sans-serif;
+			font-style: italic;
+			font-weight: 300px;
+		}
+	</style>
+
+</head>
+
+<body>
+
+    <div class="container">
+		<h1>Hi, I love you.</h1>
+    </div>
+
+    <script>
+	WebFontConfig = {
+		google: { families: [ "Roboto:100,300,400,700" ] }
+	};
+
+	(function(d) {
+		var wf = d.createElement("script"), s = d.scripts[0];
+		wf.src = "https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js";
+		wf.async = true;
+		s.parentNode.insertBefore(wf, s);
+	})(document);
+    </script>
+
+</body>
+</html>
